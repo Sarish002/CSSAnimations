4e4b3ea738df1634f181cd6c1061a6822c3a25d8
Abhinav. S. Nair <165151802+Sarish002@users.noreply.github.com>
2025-03-12 16:21:09 +0530

    Add files via upload

---
+++ Animate.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementsByClassName("A")[0].addEventListener("click", function() {
+        document.getElementById("h2").innerHTML = "The arrangement of protons, neutrons, and electrons in an atom, is reffered to as atomic structure in chemistry.";
+    });
+
+    document.getElementsByClassName("B")[0].addEventListener("click", function() {
+        document.getElementById("h2").innerHTML = "A chemical reaction is a process in which one or more substances, also called reactants, are converted to one or more different substances, known as products.";
+    });
+
+    document.getElementsByClassName("C")[0].addEventListener("click", function() {
+        document.getElementById("h2").innerHTML = "The branch of chemistry that deals with carbon compounds (other than simple salts such as carbonates, oxides, and carbides).";
+    });
+
+    document.getElementsByClassName("D")[0].addEventListener("click", function() {
+        document.getElementById("h2").innerHTML = "Chemical substances that are defined by how they react with water and other substances.";
+    });
+
+    document.getElementsByClassName("E")[0].addEventListener("click", function() {
+        document.getElementById("h2").innerHTML = "Chemical kinetics, also known as reaction kinetics, deals with the study of the reaction rates.";
+    });
+
+    document.getElementsByClassName("F")[0].addEventListener("click", function() {
+        document.getElementById("h2").innerHTML = "The study of electron movement in an oxidation or reduction reaction at a polarized electrode surface.";
+    });
+
+    document.getElementsByClassName("G")[0].addEventListener("click", function() {
+        document.getElementById("h2").innerHTML = "A state of matter is one of the distinct forms in which matter can exist. Four states of matter are observable in everyday life: solid, liquid, gas, and plasma.";
+    });
+
+    document.getElementsByClassName("H")[0].addEventListener("click", function() {
+        document.getElementById("h2").innerHTML = "The branch of chemistry that studies compounds that typically lack carbon-hydrogen bonds, and include substances like salts, oxides, acids, bases.";
+    });
+});
---
+++ Transition1.js
@@ -0,0 +1,93 @@
+(document.getElementsByClassName('AtoE'))[0].addEventListener('click', function() {
+    if (document.getElementsByClassName('AtoE').length > 0) {
+        document.getElementById("p").innerHTML = "Absolute value<br>" +
+            "Area<br>" +
+            "Average<br>" +
+            "Base (of a parallelogram or triangle)<br>" +
+            "Base (of a prism or pyramid)<br>" +
+            "Box plot<br>" +
+            "Categorical data<br>" +
+            "Center<br>" +
+            "Coefficient<br>" +
+            "Common factor<br>" +
+            "Common multiple<br>" +
+            "Compose<br>" +
+            "Coordinate plane<br>" +
+            "Cubed<br>" +
+            "Decompose<br>" +
+            "Dependent variable<br>" +
+            "Distribution<br>" +
+            "Dot plot<br>" +
+            "Double number line diagram<br>" +
+            "Edge<br>" +
+            "Equivalent expressions<br>" +
+            "Equivalent ratios<br>" +
+            "Exponent";
+    }
+});
+
+(document.getElementsByClassName('FtoJ'))[0].addEventListener('click', function() {
+    if (document.getElementsByClassName('FtoJ').length > 0) {
+        document.getElementById("p").innerHTML = "Face<br>" +
+    "Frequency<br>" +
+    "Greatest common factor<br>" +
+    "Height (of a parallelogram or triangle)<br>" +
+    "Histogram<br>" +
+    "Independent variable<br>" +
+    "Interquartile range (IQR)<br>";
+    }
+});
+
+(document.getElementsByClassName('KtoO '))[0].addEventListener('click', function() {
+    if (document.getElementsByClassName('AtoE').length > 0) {
+        document.getElementById("p").innerHTML = "Least common multiple<br>" +
+    "Long division<br>" +
+    "Mean<br>" +
+    "Mean absolute deviation (MAD)<br>" +
+    "Measure of center<br>" +
+    "Median<br>" +
+    "Meters per second<br>" +
+    "Negative number<br>" +
+    "Net<br>" +
+    "Numerical data<br>" +
+    "Opposite<br>" +
+    "Opposite vertex";
+    }
+});
+
+(document.getElementsByClassName('PtoT'))[0].addEventListener('click', function() {
+    if (document.getElementsByClassName('PtoT').length > 0) {
+        document.getElementById("p").innerHTML = "Parallelogram<br>" +
+                "Percent<br>" +
+                "Percentage<br>" +
+                "Polygon<br>" +
+                "Polyhedron (polyhedra)<br>" +
+                "Positive number<br>" +
+                "Prism<br>" +
+                "Pyramid<br>" +
+                "Quadrant<br>" +
+                "Quadrilateral<br>" +
+                "Ratio<br>" +
+                "Rational number<br>" +
+                "Reciprocal<br>" +
+                "Sign<br>" +
+                "Solution to an equation<br>" +
+                "Speed<br>" +
+                "Squared<br>" +
+                "Surface area<br>" +
+                "Table<br>" +
+                "Term"
+    }
+});
+
+(document.getElementsByClassName('UtoZ'))[0].addEventListener('click', function() {
+    if (document.getElementsByClassName('UtoZ').length > 0) {
+        document.getElementById("p").innerHTML = "Unit price<br>" +
+                "Unit rate<br>" +
+                "Variability<br>" +
+                "Variable<br>" +
+                "Vertex<br>" +
+                "Volume";
+    }
+});
+// From Copilot: Lesson --> Event Listener;
